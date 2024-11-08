2dec4efcbec9530f86e88a0e3531fb1a5cf5cd68
__yoon_a <150921809+jangyoona@users.noreply.github.com>
2024-11-08 21:45:44 +0900

    rollback

--- src/main/java/kr/co/shortenurlservice/application/SimpleShortenUrlService.java
+++ src/main/java/kr/co/shortenurlservice/application/SimpleShortenUrlService.java
@@ -79,7 +79,6 @@ public class SimpleShortenUrlService {
                 return shortenUrlKey;
         }
 
-        // throw new LackOfShortenUrlKeyException();
         throw new RuntimeException();
     }
 
